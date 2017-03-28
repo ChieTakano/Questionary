df6b506a521becc339e6afec726fa65d9f0914cd
ChieTakano <sss.0s1717@gmail.com>
2017-03-28 23:09:02 +0000

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
-get 'questionary_results/calc/:id', to: 'questionary_results#calc'
-	get 'questionary_choices/new/:id', to: 'questionary_choices#new'
-	get 'questionary_items/new/:id', to: 'questionary_items#new'
-	post 'questionaries/:id', to: 'questionaries#sendform'
+ Rails.application.routes.draw do
+  get 'questionary_results/calc/:id', to: 'questionary_results#calc'
+  get 'questionary_choices/new/:id', to: 'questionary_choices#new'
+  get 'questionary_items/new/:id', to: 'questionary_items#new'
+  post 'questionaries/:id', to: 'questionaries#sendform'
+  resources :questisults
+  resources :questionary_choices
+  resources :questionary_items
+  resources :questionaries
 
-	resources :questionary_results
-	resources :questionary_choices
-	resources :questionary_items
-	resources :questionaries
 
+  resources :answers
+  resources :questions
 
-	resources :answers
-	resources :questions
+  resources :mycontacts
 
-	resources :mycontacts
+end
